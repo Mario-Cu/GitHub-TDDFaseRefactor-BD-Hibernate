e80921c62fe08d4af1bbcb93262e7d9b6c7c8f86
mariocobreros <mario.cobreros@estudiantes.uva.es>
2023-11-17 20:27:57 +0100

    Alcanzado 100% cobertura en la implementacion de F4

--- src/main/java/uva/tds/practica2_grupo8/Sistema.java
+++ src/main/java/uva/tds/practica2_grupo8/Sistema.java
@@ -4,7 +4,8 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Sistema {
-	
+	ArrayList<Recorrido> recorridos;
+	ArrayList<Billete> billetes;
 	/**
 	 * @author marcobr (Mario Cobreros del Caz)
 	 * @author mardano (Mario Danov Ivanov)
@@ -12,14 +13,15 @@ public class Sistema {
 	 */
 
 	public Sistema() {
-		
+		this.recorridos = new ArrayList<Recorrido>();
+		this.billetes  = new ArrayList<Billete>();
 	}
 	/**
 	 * Devuelve el arraylist que guardara los recorridos
 	 * @return arraylist que guardara los recorridos
 	 */
 	public ArrayList<Recorrido> getRecorridos(){
-		return null;
+		return recorridos;
 	}
 	
 	/**
@@ -27,7 +29,7 @@ public class Sistema {
 	 * @return arraylist que guardara los billetes
 	 */
 	public ArrayList<Billete> getBilletes(){
-		return null;
+		return billetes;
 	}
 	
 	/**
@@ -55,16 +57,63 @@ public class Sistema {
 	 * @throws IllegalArgumentException cuando se añade un recorrido nulo
 	 */
 	public void añadirRecorrido(Recorrido recorrido) {
+		if(recorrido == null) {
+			throw new IllegalArgumentException("El recorrido no puede ser nulo");
+		}
+		for(Recorrido item : recorridos) {
+			if(item.getId() == recorrido.getId()){
+				throw new IllegalStateException("El recorrido que intentas añadir ya existe en el sistema");
+			}
+		}
+		
+		recorridos.add(recorrido);
 	}
 	
 	
 	/**
 	 * Elimina un recorrido del arraylist de recorridos	 
 	 * @throws IllegalStateException cuando se elimina un recorrido con billetes asociados
-	 * @throws IllegalArgumentException cuando se intenta eliminar un recorrido con localizador nulo
+	 * @throws IllegalArgumentException cuando se intenta eliminar un recorrido con identificador nulo
 	 */
 	public void eliminarRecorrido(String id) {
-		
+		if(id == null) {
+			throw new IllegalArgumentException("El identificador no puede ser nulo");
+		}
+		for(Billete item : billetes) {
+			if(item.getRecorrido().getId() == id){
+				throw new IllegalStateException("El recorrido que intentas eliminar tiene un billete asociado");
+			}
+		}
+		int indiceRecorrido = getIndexRecorridoPorId(id);
+		recorridos.remove(indiceRecorrido);
+	}
+	
+	/**
+	 * Metodo privado que obtiene el indice donde se encuentra el recorrido a traves de su id
+	 * 
+	 */
+	private int getIndexRecorridoPorId(String id) {
+		int indice = -1;
+		for(Recorrido item : recorridos) {
+			if(item.getId() == id) {
+				indice = recorridos.indexOf(item);
+			}
+		}
+		return indice;
+	}
+	
+	/**
+	 * Metodo privado que obtiene el recorrido a traves de su id
+	 * 
+	 */
+	private Recorrido getRecorridoPorId(String id) {
+		Recorrido solucion = null;
+		for(Recorrido item : recorridos) {
+			if(item.getId() == id) {
+				solucion = item;
+			}
+		}
+		return solucion;
 	}
 	
 	
@@ -72,11 +121,18 @@ public class Sistema {
 	 * Actualiza la fecha de un recorrido
 	 * @param id identificador del recorrido
 	 * @param fecha fecha nueva del reocorrido
-	 * @throws IllegalArgumentException cuando localizador es nulo
-	 * @throws IllegalArgumentException cuando fecha es nula
+	 * @throws IllegalStateException cuando localizador es nulo
+	 * @throws IllegalStateException cuando fecha es nula
 	 */
 	public void actualizarFechaRecorrido(String id,LocalDate fecha) {
-	
+		if(id == null) {
+			throw new IllegalStateException("El identificador no puede ser nulo");
+		}
+		if(fecha == null) {
+			throw new IllegalStateException("La fecha no puede ser nulo");
+		}
+		Recorrido recorrido = getRecorridoPorId(id);
+		recorrido.setFecha(fecha);
 	}
 	
 	
@@ -84,11 +140,18 @@ public class Sistema {
 	 * Actualiza la hora de un recorrido 
 	 * @param id identificador del recorrido
 	 * @param hora hora nueva del recorrido
-	 * @throws IllegalArgumentException cuando localizador es nulo
-	 * @throws IllegalArgumentException cuando hora es nula
+	 * @throws IllegalStateException cuando identificador es nulo
+	 * @throws IllegalStateException cuando hora es nula
 	 */
 	public void actualizarHoraRecorrido(String id,LocalTime hora) {
-		
+		if(id == null) {
+			throw new IllegalStateException("El identificador no puede ser nulo");
+		}
+		if(hora == null) {
+			throw new IllegalStateException("La hora no puede ser nulo");
+		}
+		Recorrido recorrido = getRecorridoPorId(id);
+		recorrido.setHora(hora);
 	}
 
 	/**
@@ -151,10 +214,14 @@ public class Sistema {
 	/**
 	 * Añade un billete al arraylist de billetes
 	 * @param billete billete a añadir al arraylists
-	 *@throws IllegalArgumentException si el billete es nulo
+	 * @throws IllegalArgumentException si el billete es nulo
 	 */
 	public void añadirBillete(Billete billete) {	
-
+		if(billete == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		billetes.add(billete);
 	}
 
 
